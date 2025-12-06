28bf243fcd084d3adaa0168624cb57f926e84c9c
henoc35 <13746485+henoc35@users.noreply.github.com>
2025-12-06 22:02:57 +0000

    Fix styling

--- src/Concerns/IsAuditable.php
+++ src/Concerns/IsAuditable.php
@@ -3,12 +3,10 @@
 namespace SoftArtisan\LaravelModelAudits\Concerns;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Schema;
 
 /**
  * Trait IsAuditable
